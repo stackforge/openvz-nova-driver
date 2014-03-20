3e262af80d540c1348d2ff29b97d69dedb7d37e2
Joe Cruz <joe.cruz@rackspace.com>
2014-03-20 17:56:56 -0500

    Container flavor settings and Vswap
    * This allows for defining exact vz configs for specific flavor, instead
      of the driver calculating them.  We can define future vz configs
      through nova flavor extra_specs (metadata) instead of adding configs
      in the driver.
    
    * Vswap has been added as the new default way of setting memory settings
    
    * Refactoring has been done to use new Container class.
    
    Change-Id: I125682bbe89cedec08f328394ebb402a5c079b6d

--- ovznovadriver/openvz/container.py
+++ ovznovadriver/openvz/container.py
@@ -16,6 +16,7 @@
 import json
 
 from nova import exception
+from nova.compute import flavors
 from nova.openstack.common import log as logging
 from oslo.config import cfg
 from ovznovadriver.localization import _
@@ -24,6 +25,14 @@ from ovznovadriver.openvz import utils as ovz_utils
 CONF = cfg.CONF
 
 LOG = logging.getLogger(__name__)
+
+
+def generate_vz_settings_from_flavor(flavor):
+
+    settings = {}
+
+    return settings
+
 
 # More of the openvz logic belongs in here, but it was hard to de-tangle it
 # from all the libraries, so a minimal subset to get the desired outcome
@@ -220,6 +229,273 @@ class OvzContainer(object):
         Remove the OpenVZ container this object represents.
         """
         ovz_utils.execute('vzctl', 'destroy', self.ovz_id, run_as_root=True)
+
+    def apply_config(self, config='basic'):
+        """
+        This adds the container root into the vz meta data so that
+        OpenVz acknowledges it as a container.  Punting to a basic
+        config for now.
+
+        Run the command:
+
+        vzctl set <ctid> --save --applyconfig <config>
+
+        This sets the default configuration file for openvz containers.  This
+        is a requisite step in making a container from an image tarball.
+
+        If this fails to run successfully an exception is raised because the
+        container this executes against requires a base config to start.
+        """
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--applyconfig', config, run_as_root=True)
+
+    def set_vz_os_hint(self, ostemplate='ubuntu'):
+        """
+        This exists as a stopgap because currently there are no os hints
+        in the image managment of nova.  There are ways of hacking it in
+        via image_properties but this requires special case code just for
+        this driver.
+
+        Run the command:
+
+        vzctl set <ctid> --save --ostemplate <ostemplate>
+
+        Currently ostemplate defaults to ubuntu.  This facilitates setting
+        the ostemplate setting in OpenVZ to allow the OpenVz helper scripts
+        to setup networking, nameserver and hostnames.  Because of this, the
+        openvz driver only works with debian based distros.
+
+        If this fails to run an exception is raised as this is a critical piece
+        in making openvz run a container.
+        """
+
+        # This sets the distro hint for OpenVZ to later use for the setting
+        # of resolver, hostname and the like
+
+        # TODO(imsplitbit): change the ostemplate default value to a flag
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--ostemplate', ostemplate, run_as_root=True)
+
+    def set_numflock(self, max_file_descriptors):
+        """
+        Run the command:
+
+        vzctl set <ctid> --save --numflock <number>
+        """
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--numflock', max_file_descriptors,
+                          run_as_root=True)
+
+    def set_numfiles(self, max_file_descriptors):
+        """
+        Run the command:
+
+        vzctl set <ctid> --save --numfile <number>
+        """
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--numfile', max_file_descriptors,
+                          run_as_root=True)
+
+    # TODO(jcru) extract calculations from here and only pass tcp_sockets to
+    # function.
+    def set_numtcpsock(self, memory_mb):
+        """
+        Run the commnand:
+
+        vzctl set <ctid> --save --numtcpsock <number>
+
+        :param instance:
+        :return:
+        """
+        try:
+            tcp_sockets = CONF.ovz_numtcpsock_map[str(memory_mb)]
+        except (ValueError, TypeError, KeyError, cfg.NoSuchOptError):
+            LOG.error(_('There was no acceptable tcpsocket number found '
+                        'defaulting to %s') % CONF.ovz_numtcpsock_default)
+            tcp_sockets = CONF.ovz_numtcpsock_default
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--numtcpsock', tcp_sockets, run_as_root=True)
+
+    def set_vmguarpages(self, num_pages):
+        """
+        Set the vmguarpages attribute for a container.  This number represents
+        the number of 4k blocks of memory that are guaranteed to the container.
+        This is what shows up when you run the command 'free' in the container.
+
+        Run the command:
+
+        vzctl set <ctid> --save --vmguarpages <num_pages>
+
+        If this fails to run then an exception is raised because this affects
+        the memory allocation for the container.
+        """
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--vmguarpages', num_pages, run_as_root=True)
+
+    def set_privvmpages(self, num_pages):
+        """
+        Set the privvmpages attribute for a container.  This represents the
+        memory allocation limit.  Think of this as a bursting limit.  For now
+        We are setting to the same as vmguarpages but in the future this can be
+        used to thin provision a box.
+
+        Run the command:
+
+        vzctl set <ctid> --save --privvmpages <num_pages>
+
+        If this fails to run an exception is raised as this is essential for
+        the running container to operate properly within it's memory
+        constraints.
+        """
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--privvmpages', num_pages, run_as_root=True)
+
+    def set_kmemsize(self, kmem_barrier, kmem_limit):
+        """
+        Set the kmemsize attribute for a container.  This represents the
+        amount of the container's memory allocation that will be made
+        available to the kernel.  This is used for tcp connections, unix
+        sockets and the like.
+
+        This runs the command:
+
+        vzctl set <ctid> --save --kmemsize <barrier>:<limit>
+
+        If this fails to run an exception is raised as this is essential for
+        the container to operate under a normal load.  Defaults for this
+        setting are completely inadequate for any normal workload.
+        """
+        kmemsize = '%d:%d' % (kmem_barrier, kmem_limit)
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--kmemsize', kmemsize, run_as_root=True)
+
+    def set_cpuunits(self, units):
+        """
+        Set the cpuunits setting for the container.  This is an integer
+        representing the number of cpu fair scheduling counters that the
+        container has access to during one complete cycle.
+
+        Run the command:
+
+        vzctl set <ctid> --save --cpuunits <units>
+
+        If this fails to run an exception is raised because this is the secret
+        sauce to constraining each container within it's subscribed slice of
+        the host node.
+        """
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--cpuunits', units, run_as_root=True)
+
+    def set_cpulimit(self, cpulimit):
+        """
+        This is a number in % equal to the amount of cpu processing power
+        the container gets.  NOTE: 100% is 1 logical cpu so if you have 12
+        cores with hyperthreading enabled then 100% of the whole host machine
+        would be 2400% or --cpulimit 2400.
+
+        Run the command:
+
+        vzctl set <ctid> --save --cpulimit <cpulimit>
+
+        If this fails to run an exception is raised because this is the secret
+        sauce to constraining each container within it's subscribed slice of
+        the host node.
+        """
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--cpulimit', cpulimit, Run_as_root=True)
+
+    # TODO(jcru) CALC extract self.utility
+    def set_cpus(self, vcpus):
+        """
+        The number of logical cpus that are made available to the container.
+        Default to showing 2 cpus to each container at a minimum.
+
+        Run the command:
+
+        vzctl set <ctid> --save --cpus <num_cpus>
+
+        If this fails to run an exception is raised because this limits the
+        number of cores that are presented to each container and if this fails
+        to set *ALL* cores will be presented to every container, that be bad.
+        """
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save', '--cpus',
+                          vcpus, run_as_root=True)
+
+    def set_ioprio(self, ioprio):
+        """
+        Set the IO priority setting for a given container.  This is represented
+        by an integer between 0 and 7.
+        Run the command:
+
+        vzctl set <ctid> --save --ioprio <iopriority>
+
+        If this fails to run an exception is raised because all containers are
+        given the same weight by default which will cause bad performance
+        across all containers when there is input/output contention.
+        """
+        # The old algorithm made it impossible to distinguish between a
+        # 512MB container and a 2048MB container for IO priority.  We will
+        # for now follow a simple map to create a more non-linear
+        # relationship between the flavor sizes and their IO priority groups
+
+        # The IO priority of a container is grouped in 1 of 8 groups ranging
+        # from 0 to 7.  We can calculate an appropriate value by finding out
+        # how many ovz_memory_unit_size chunks are in the container's memory
+        # allocation and then using python's math library to solve for that
+        # number's logarithm.
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--ioprio', ioprio, run_as_root=True)
+
+    def set_diskspace(self, soft_limit, hard_limit):
+        """
+        Implement OpenVz disk quotas for local disk space usage.
+        This method takes a soft and hard limit.  This is also the amount
+        of diskspace that is reported by system tools such as du and df inside
+        the container.  If no argument is given then one will be calculated
+        based on the values in the instance_types table within the database.
+
+        Run the command:
+
+        vzctl set <ctid> --save --diskspace <soft_limit:hard_limit>
+
+        If this fails to run an exception is raised because this command
+        limits a container's ability to hijack all available disk space.
+        """
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--diskspace', '%s:%s' % (soft_limit, hard_limit),
+                          run_as_root=True)
+
+    def set_vswap(self, ram, swap):
+        """
+        Implement OpenVz vswap memory management model (The other being user
+        beancounters).
+
+        The sum of physpages_limit and swappages_limit limits the maximum
+        amount of memory which can be used by a container. When physpages limit
+        is reached, memory pages belonging to the container are pushed out to
+        so called virtual swap (vswap). The difference between normal swap and
+        vswap is that with vswap no actual disk I/O usually occurs. Instead, a
+        container is artificially slowed down, to emulate the effect of the
+        real swapping. Actual swap out occurs only if there is a global memory
+        shortage on the system.
+
+        Run the command:
+
+        vzctl set <ctid> --ram <physpages_limit> --swap <swappages_limit>
+        """
+
+        ovz_utils.execute('vzctl', 'set', self.ovz_id, '--save',
+                          '--ram', ram, '--swap', swap, run_as_root=True)
+
 
 class OvzContainers(object):
 
